a09473d89876fd302fbb9d15aa390f0bffeacc55
torpedothomas <50775654+torpedothomas@users.noreply.github.com>
2019-06-08 19:31:30 +0200

    Update Tisch.java

--- Restaurant/src/restaurant/Tisch.java
+++ Restaurant/src/restaurant/Tisch.java
@@ -1,14 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package restaurant;
+import java.util.Random;
 
 /**
  *
- * @author Thomas Weber
+ * @author Katharina Lainer
  */
 public class Tisch {
-    
+public class Tisch{
+
+    private int stuhlzahl;
+    private int tischnr;
+    private int besetzt;
+    private Random number;
+
+    public Tisch (int sz, int nr){
+
+        stuhlzahl = sz;
+        tischnr = nr;
+        besetzt = 0;
+        number = new Random();
+    }
+
+    public boolean gaesteKommen (int anzahl){
+
+        if(stuhlzahl < anzahl){
+
+            return false;
+        }else{
+
+            besetzt = anzahl;
+            return true;
+        }
+    }
+
+    public void gaesteGehen(){
+
+        besetzt = 0;
+    }
+
+    public int stuhlZahlGeben(){
+
+        return stuhlzahl;
+    }
+
+    public int gaesteZahlGeben(){
+
+        return besetzt;
+    }
+
+    public void datenAusgeben(){
+
+        System.out.println("Tisch " + tischnr + ": Gästezahl = " + besetzt + " von " + stuhlzahl);
+    }
+
+    public int tischNummerGeben(){
+
+        return tischnr;
+    }
 }
